561c8208ed474dd9d75b900d81717612fa51046c
qingxo <qingxo@sina.com>
2017-10-16 17:49:43 +0800

    1、样式调整 2、新增首页component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,10 +5,12 @@ import {HomeComponent} from './home.component';
 import {TabsComponent} from './tabs';
 import {PatientsComponent} from './patients';
 import {SleepAnalysisComponent} from './sleep-analysis';
+import {MainPageComponent} from './main-page'
 const homeRoutes: Routes = [
   { path: '', component: HomeComponent,
     children:[
-      { path: '', redirectTo: '/home/demo', pathMatch: 'full' },
+      { path: '', redirectTo: '/home/main', pathMatch: 'full' },
+      {path:'main',component:MainPageComponent},
       {path:'demo',component:DemoComponent},
       {path:'patients',component:PatientsComponent},
       {path:'sleepanalysis',component:SleepAnalysisComponent}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,12 +8,13 @@ import { TabsComponent } from './tabs/tabs.component';
 import { PatientsComponent } from './patients/patients.component';
 import { SleepAnalysisComponent } from './sleep-analysis/sleep-analysis.component';
 import { TtsComponent } from './tts/tts.component';
+import { MainPageComponent } from './main-page/main-page.component';
 @NgModule({
   imports: [
     CommonModule,
     NgZorroAntdModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent,DemoComponent, TabsComponent, PatientsComponent, SleepAnalysisComponent, TtsComponent]
+  declarations: [HomeComponent,DemoComponent, TabsComponent, PatientsComponent, SleepAnalysisComponent, TtsComponent, MainPageComponent]
 })
 export class HomeModule { }
