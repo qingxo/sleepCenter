0b14b94abfe911e987ad3bf37c8de5f3db3e87e9
taoqun <taoqun@317hu.com>
2017-10-17 14:51:33 +0800

    样式调整

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-home',
@@ -8,43 +8,43 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
-  homeShow:boolean = true;
-  otherShow:any ;
-  activePageName:string = '';
-  activePageUrl:string='';
-  tabLength:number =1;
-  constructor(public route:ActivatedRoute) { }
+  homeShow: boolean = true;
+  otherShow: any;
+  activePageName: string = '';
+  activePageUrl: string = '';
+  tabLength: number = 1;
+  constructor(public route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
-  toggleChoose(num,ev){
-    if(num === 1){
-        this.homeShow = !this.homeShow;
-        this.otherShow.target.className = 'ant-menu-item';
-    }else{
+  toggleChoose(num, ev) {
+    if (num === 1) {
+      this.homeShow = !this.homeShow;
+      this.otherShow.target.className = 'ant-menu-item';
+    } else {
       this.otherShow = ev;
       this.homeShow = false;
     }
 
-    this.activePageName  =ev.target.innerText;
-    setTimeout(()=>{
-      this.activePageUrl =this.route['_futureSnapshot']['_routerState'].url
-    },1)
+    this.activePageName = ev.target.innerText;
+    setTimeout(() => {
+      this.activePageUrl = this.route['_futureSnapshot']['_routerState'].url
+    }, 1)
 
   }
 
-  menuChange(info){
+  menuChange(info) {
     $(".ant-menu-item").removeClass('ant-menu-item-selected');
-    $('.ant-menu-item').each((index,el)=>{
-      if($(el).attr('routerlink')===info.url){
+    $('.ant-menu-item').each((index, el) => {
+      if ($(el).attr('routerlink') === info.url) {
         $(el).addClass('ant-menu-item-selected');
-
       }
     })
+    this.activePageName = info.name;
   }
 
-  tabLengh(num){
+  tabLengh(num) {
     this.tabLength = num;
   }
 }
