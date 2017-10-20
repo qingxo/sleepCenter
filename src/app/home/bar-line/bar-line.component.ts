eb50808123805c4a83c25778482765e43e5166e9
taoqun <taoqun@317hu.com>
2017-10-20 14:59:15 +0800

    多体征监测模块完善

--- src/app/home/bar-line/bar-line.component.ts
+++ src/app/home/bar-line/bar-line.component.ts
@@ -10,13 +10,89 @@ export class BarLineComponent implements OnInit {
   private nothingFlag = false;
   @Input() topTitle = '';
   @Input() serisesData: Array<any> = [];
-  @Input() echartsStyle: any = { 'height': '180px' };
+  @Input() echartsStyle: any = { 'height': '230px' };
   @Input() name: string = '测试';
   @Input() imageSrc: string = "xxx";
   @ViewChild('tt') el: ElementRef;
   constructor() { }
 
   ngOnInit() {
+    this.option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      color: ['#3D70E4', '#9879FF'],
+      legend: {
+        orient: 'veritcal',
+        right: '10%',
+        top: '45%',
+        data: ['直接访问', '间接访问']
+      },
+      grid: {
+        left: '3%',
+        right: '20%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          data: [10, 52, 200, 334, 390, 330, 220],
+          label: {
+            normal: {
+              show: true,
+              color: '#6f7b91',
+              position: 'top',
+              textStyle: {
+                fontSize: '12'
+              }
+            }
+          },
+          itemStyle: {
+            normal: {
+              barBorderRadius: [15, 15, 0, 0]
+            }
+          },
+        }, {
+          name: '间接访问',
+          type: 'bar',
+          data: [20, 12, 100, 304, 90, 30, 120],
+          label: {
+            normal: {
+              show: true,
+              color: '#6f7b91',
+              position: 'top',
+              textStyle: {
+                fontSize: '12'
+              }
+            }
+          },
+          itemStyle: {
+            normal: {
+              barBorderRadius: [15, 15, 0, 0]
+            }
+          },
+        }
+      ]
+    }
   }
 
 }
